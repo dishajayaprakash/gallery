ef83ca06b31ff47623b82e3825800cd57d30e9c6
dishajayaprakash <dishajayaprakash94@gmail.com>
2023-04-17 15:40:17 +0530

    17th April

--- gallery/views.py
+++ gallery/views.py
@@ -4,3 +4,7 @@ from gallery.models import gallery
 def disp(request):
     results=gallery.objects.all()
     return render(request, 'pictures.html', {'gallery': results})
+
+def home_page(request):
+    context = {"title": "Home Page"}
+    return render(request, "home.html", context)
